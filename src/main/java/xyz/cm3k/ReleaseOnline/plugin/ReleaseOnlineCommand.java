0bc0ee0b9cf0724b810ea31d388f150ca8f947a1
Thomas CashMan <48776071+cm3k@users.noreply.github.com>
2023-02-22 23:33:08 +0000

    changed output of /releaseonline

--- src/main/java/xyz/cm3k/ReleaseOnline/plugin/ReleaseOnlineCommand.java
+++ src/main/java/xyz/cm3k/ReleaseOnline/plugin/ReleaseOnlineCommand.java
@@ -9,9 +9,9 @@ public class ReleaseOnlineCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         commandSender.sendMessage("=== " + ChatColor.RED + "Release" + ChatColor.BLUE + "Online" + ChatColor.WHITE + " Plugin ===");
-        commandSender.sendMessage("Version " + ChatColor.YELLOW + ReleaseOnlinePlugin.version + ChatColor.WHITE);
-        commandSender.sendMessage("Licenced under MPL-2.0 (Excluding GPL-3.0 BukkitAPI JAR)");
-        commandSender.sendMessage("https://github.com/ReleaseOnline/plugin");
+        commandSender.sendMessage("Version: " + ChatColor.YELLOW + ReleaseOnlinePlugin.version + ChatColor.WHITE);
+        commandSender.sendMessage("Licence: MPL-2.0 (Excluding GPL-3.0 BukkitAPI JAR)");
+        commandSender.sendMessage("Source: https://github.com/ReleaseOnline/plugin");
         return false;
     }
 }
